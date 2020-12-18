327b212fca5234a135473d42f5a0f2478f207bcd
Maxim <linqcod@yandex.ru>
2020-12-18 10:56:03 +0300

    final-version-beta-0.1

--- app/src/main/java/com/linqcod/codingquest/MainActivity.java
+++ app/src/main/java/com/linqcod/codingquest/MainActivity.java
@@ -15,12 +15,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button startBtn = findViewById(R.id.startBtn);
+        Button exitBtn = findViewById(R.id.exitBtn);
 
         startBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), QuestActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        exitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
             }
         });
     }
